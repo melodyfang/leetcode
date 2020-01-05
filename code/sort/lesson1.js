bcb06615d1bd5ce6ac634d6ce3123295c1057235
melody <melody.gan@geekbang.com>
2020-01-05 13:32:35 +0800

    41. 缺失的第一个正数，使用选择排序解决问题

--- code/sort/lesson1.js
+++ code/sort/lesson1.js
@@ -2,24 +2,20 @@
  * 选择排序
  */
 
-[3,5,6,9,1,2]
+// [3,5,6,9,1,2]
 
-[1,5,6,9,3,2]
+// [1,5,6,9,3,2]
 
 export default (arr) => {
-  let idx = 0
   for(let i = 0; i < arr.length; i++) {
-    let min = Number.MAX_SAFE_INTEGER
     // 找到这一轮比较的最小值
     for(let j = i; j < arr.length; j++) {
-      if (min > arr[j]) {
-        min = arr[j]
-        idx = j
+      if (arr[i] > arr[j]) {
+        tmp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = tmp
       }
     }
-    let tmp = arr[i]
-    arr[i] = min
-    arr[idx] = tmp
   }
   return arr
 }
--- code/sort/lesson5.js
+++ code/sort/lesson5.js
@@ -28,20 +28,58 @@
 //   return min
 // };
 
-// 第2种思路
+/**
+ * 第2种思路
+ * 缺点：并不需要全部排序
+ * @param {*} arr 
+ */
+// export default function(arr) {
+//   // 过滤掉非正整数
+//   arr = arr.filter(item => item > 0)
+//   arr.sort((a, b) => a - b)
+//   if (arr[0] !== 1) {
+//     return 1
+//   }
+//   for(let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i + 1] - arr[i] > 1) {
+//       return arr[i] + 1
+//     }
+//   }
+//   // 说明数组所有元素都是连续的
+//   return arr.pop() + 1
+// };
+
+
+/**
+ * 第3种思路
+ * 使用选择排序
+ * @param {*} arr 
+ */
 export default function(arr) {
   // 过滤掉非正整数
   arr = arr.filter(item => item > 0)
-  arr.sort((a, b) => a - b)
-  if (arr[0] !== 1) {
+  if (!arr.length) {
     return 1
   }
-  for(let i = 0; i < arr.length - 1; i++) {
-    if (arr[i + 1] - arr[i] > 1) {
-      return arr[i] + 1
+  let tmp
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = i; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        tmp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = tmp
+      }
+    }  
+    if (i > 0) {
+      if (arr[i] - arr[i - 1] > 1) {
+        return arr[i - 1] + 1
+      }
+    } else {
+      if (arr[i] !== 1) {
+        return 1
+      }
     }
   }
   // 说明数组所有元素都是连续的
   return arr.pop() + 1
 };
-
